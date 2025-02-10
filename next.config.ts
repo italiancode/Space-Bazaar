f9a63b45569a421d7a8b6ab510660c23734462ff
italiancode <italianloader@gmail.com>
2025-02-10 18:06:52 +0100

    Something just got updated!

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,24 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
-    domains: ["space-bazaar.vercel.app", "shop.spacex.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "space-bazaar.vercel.app",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "shop.spacex.com",
-        port: "",
-        pathname: "/cdn/**",
-      },
+    domains: [
+      'space-bazaar.vercel.app',
+      'shop.spacex.com'
     ],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'space-bazaar.vercel.app',
+      port: '',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'shop.spacex.com',
+      port: '',
+      pathname: '/cdn/**',
+    }],
   },
-
+  swcMinify: true,
   reactStrictMode: true,
   poweredByHeader: false,
   compiler: {
