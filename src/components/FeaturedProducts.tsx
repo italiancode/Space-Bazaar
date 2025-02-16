db3e260e358d6d1594c67470ae69125aabaf7257
italiancode <italianloader@gmail.com>
2025-02-16 13:38:36 +0100

    Something just got updated!

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -44,7 +44,7 @@ export default function FeaturedProducts() {
         >
           Featured SpaceX Merch
         </motion.h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {featuredProducts.map((product, index) => (
             <div key={index} className="h-fit">
               <ProductCard key={index} product={product} />
